7ba0bfc43418da04e26733ecc310653d61d3810f
HarrisonGroom <89763078+HarrisonGroom@users.noreply.github.com>
2022-11-10 11:00:49 -0700

    Update pricing.js

--- lambda/pricing.js
+++ lambda/pricing.js
@@ -1,5 +1,6 @@
 module.exports = [
   'parking will cost three dollars and seventy five an hour',
   'parking is free today',
-  'parking will range from free to 2 dollars depending upon spaces'
+  'parking will range from free to 2 dollars depending upon spaces',
+  'ther is no parking today'
 ];
